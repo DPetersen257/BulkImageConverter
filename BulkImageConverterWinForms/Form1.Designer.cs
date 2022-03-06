a3eb882a44346401d68dd6d8f8d992ae0142589d
Dpetersen257 <dallas.ptrsn@gmail.com>
2022-03-05 23:32:29 -0700

    Add project files.

---
+++ BulkImageConverterWinForms/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace BulkImageConverterWinForms;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBoxFromExtension = new System.Windows.Forms.ComboBox();
+            this.comboBoxToExtension = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.checkBoxRecurse = new System.Windows.Forms.CheckBox();
+            this.checkBoxDelete = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(202, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBoxFromExtension
+            // 
+            this.comboBoxFromExtension.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBoxFromExtension.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBoxFromExtension.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFromExtension.FormattingEnabled = true;
+            this.comboBoxFromExtension.Items.AddRange(new object[] {
+            ".bmp",
+            ".png"});
+            this.comboBoxFromExtension.Location = new System.Drawing.Point(202, 151);
+            this.comboBoxFromExtension.Name = "comboBoxFromExtension";
+            this.comboBoxFromExtension.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxFromExtension.TabIndex = 2;
+            // 
+            // comboBoxToExtension
+            // 
+            this.comboBoxToExtension.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBoxToExtension.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBoxToExtension.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxToExtension.FormattingEnabled = true;
+            this.comboBoxToExtension.Items.AddRange(new object[] {
+            "JPG"});
+            this.comboBoxToExtension.Location = new System.Drawing.Point(475, 151);
+            this.comboBoxToExtension.Name = "comboBoxToExtension";
+            this.comboBoxToExtension.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxToExtension.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "From Extension";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(493, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To Extension";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(521, 90);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 1;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.ButtonBrowse_Click);
+            // 
+            // checkBoxRecurse
+            // 
+            this.checkBoxRecurse.AutoSize = true;
+            this.checkBoxRecurse.Location = new System.Drawing.Point(341, 196);
+            this.checkBoxRecurse.Name = "checkBoxRecurse";
+            this.checkBoxRecurse.Size = new System.Drawing.Size(114, 19);
+            this.checkBoxRecurse.TabIndex = 7;
+            this.checkBoxRecurse.Text = "Recurse Subdirs?";
+            this.checkBoxRecurse.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDelete
+            // 
+            this.checkBoxDelete.AutoSize = true;
+            this.checkBoxDelete.Location = new System.Drawing.Point(341, 221);
+            this.checkBoxDelete.Name = "checkBoxDelete";
+            this.checkBoxDelete.Size = new System.Drawing.Size(112, 19);
+            this.checkBoxDelete.TabIndex = 8;
+            this.checkBoxDelete.Text = "Delete Old Files?";
+            this.checkBoxDelete.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxLog, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 73.33334F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.checkBoxDelete);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.checkBoxRecurse);
+            this.panel1.Controls.Add(this.buttonBrowse);
+            this.panel1.Controls.Add(this.comboBoxFromExtension);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.comboBoxToExtension);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(794, 324);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxLog.Location = new System.Drawing.Point(3, 333);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxLog.Size = new System.Drawing.Size(794, 114);
+            this.textBoxLog.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private FolderBrowserDialog folderBrowserDialog1;
+    private TextBox textBox1;
+    private ComboBox comboBoxFromExtension;
+    private ComboBox comboBoxToExtension;
+    private Label label1;
+    private Label label2;
+    private Button button1;
+    private Button buttonBrowse;
+    private CheckBox checkBoxRecurse;
+    private CheckBox checkBoxDelete;
+    private TableLayoutPanel tableLayoutPanel1;
+    private Panel panel1;
+    private TextBox textBoxLog;
+}
---
+++ BulkImageConverterWinForms/Form1.cs
@@ -0,0 +1,115 @@
+using System.Drawing.Imaging;
+
+namespace BulkImageConverterWinForms;
+
+public partial class Form1 : Form
+{
+    public Form1() => InitializeComponent();
+
+    private void ButtonBrowse_Click(object sender, EventArgs e)
+    {
+        folderBrowserDialog1.ShowDialog();
+        textBox1.Text = folderBrowserDialog1.SelectedPath;
+    }
+
+    private void Button1_Click(object sender, EventArgs e)
+    {
+        textBoxLog.Text = "";
+        if (!ValidateForm())
+            return;
+
+        string directory = textBox1.Text;
+        int converted = 0;
+        string fromExtension = comboBoxFromExtension.SelectedItem.ToString();
+        switch (comboBoxToExtension.SelectedItem)
+        {
+            case "JPG":
+                converted = ConvertFilesToJPG(directory, fromExtension, checkBoxRecurse.Checked, checkBoxDelete.Checked);
+                break;
+        }
+
+        AppendLog($"Converted: {converted} files.", Color.Green);
+    }
+
+    private int ConvertFilesToJPG(string directory, string fromExtension, bool recurseSubdirectories, bool deleteOld)
+    {
+        int converted = 0;
+        foreach (string file in Directory.EnumerateFiles(directory, $"*{fromExtension}", new EnumerationOptions() { RecurseSubdirectories = recurseSubdirectories }))
+        {
+            try
+            {
+                string jpgFile = ConvertToJpg(file);
+                // Copy the file access/creation times from the original file to the new file.
+                File.SetCreationTime(jpgFile, File.GetCreationTime(file));
+                File.SetLastWriteTime(jpgFile, File.GetLastWriteTime(file));
+                File.SetLastAccessTime(jpgFile, File.GetLastAccessTime(file));
+                File.SetAttributes(jpgFile, File.GetAttributes(file));
+
+                if (deleteOld)
+                    File.Delete(file);
+                converted++;
+                AppendLog($"Converted: {file}", Color.Black);
+            }
+            catch (Exception ex)
+            {
+                AppendLog($"Failed converting: {file}", Color.Red);
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+        return converted;
+    }
+
+    //This method appends a string to textboxLog in the specified color
+    private void AppendLog(string text, Color color)
+    {
+        Color previousColor = textBoxLog.ForeColor;
+
+        textBoxLog.SelectionStart = textBoxLog.TextLength;
+        textBoxLog.SelectionLength = 0;
+        textBoxLog.ForeColor = color;
+        if (textBoxLog.Text.Length > 0)
+            textBoxLog.AppendText(Environment.NewLine);
+        textBoxLog.AppendText(text);
+        textBoxLog.ForeColor = previousColor;
+    }
+
+    private bool ValidateForm()
+    {
+        if (comboBoxFromExtension.SelectedIndex == -1)
+        {
+            MessageBox.Show("Select FROM extension");
+            return false;
+        }
+        if (comboBoxToExtension.SelectedIndex == -1)
+        {
+            MessageBox.Show("Select TO extension");
+            return false;
+        }
+        if (string.IsNullOrWhiteSpace(textBox1.Text) || !Directory.Exists(textBox1.Text))
+        {
+            MessageBox.Show("Directory not valid");
+            return false;
+        }
+
+        return true;
+    }
+
+    // This method will prompt the user for a directory path
+    public static string? GetDirectoryPathFromUser()
+    {
+        Console.WriteLine("Enter a directory path");
+        string? directoryPath = Console.ReadLine();
+        return (Directory.Exists(directoryPath)) ? directoryPath : null;
+    }
+
+    //This method converts a .bmp file into a .jpg file
+    public static string ConvertToJpg(string filePath)
+    {
+        string jpgFilePath = Path.ChangeExtension(filePath, ".jpg");
+        #pragma warning disable CA1416 // Validate platform compatibility
+        using var bitmap = new Bitmap(filePath);
+        bitmap.Save(jpgFilePath, ImageFormat.Jpeg);
+        #pragma warning restore CA1416 // Validate platform compatibility
+        return jpgFilePath;
+    }
+}
